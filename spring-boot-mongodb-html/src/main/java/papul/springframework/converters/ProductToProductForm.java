d1ba059817bae00c4a2a93cb34b47d6956b99db9
Papul Ghosh <30683617+Papul-Ghosh@users.noreply.github.com>
2022-05-21 15:50:59 +0200

    first commit to main

---
+++ spring-boot-mongodb-html/src/main/java/papul/springframework/converters/ProductToProductForm.java
@@ -0,0 +1,22 @@
+package papul.springframework.converters;
+
+import papul.springframework.commands.ProductForm;
+import papul.springframework.domain.Product;
+import org.springframework.core.convert.converter.Converter;
+import org.springframework.stereotype.Component;
+
+/**
+ * Created by jt on 1/10/17.
+ */
+@Component
+public class ProductToProductForm implements Converter<Product, ProductForm> {
+    @Override
+    public ProductForm convert(Product product) {
+        ProductForm productForm = new ProductForm();
+        productForm.setId(product.getId().toHexString());
+        productForm.setDescription(product.getDescription());
+        productForm.setPrice(product.getPrice());
+        productForm.setManufacturer(product.getManufacturer());
+        return productForm;
+    }
+}
